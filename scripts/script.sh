ebcecbd42530cfb036600b416ba0fc8bfbcee2fc
Nicolas Wagner <nicolas.wagner@epfl.ch>
2024-05-29 18:59:20 +0200

    add lr

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 wandb_project dataset_name trust num_clients"
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 wandb_project dataset_name trust num_clients lr"
     exit 1
 fi
 
@@ -9,11 +9,12 @@ wandb_project=$1
 dataset_name=$2
 trust=$3
 num_clients=$4
+lora_rank=$5
 
-for seed in {1..10}; do
+for seed in {1..5}; do
     echo "---------------- New sample with seed: $seed ----------------"
 
-    python -W ignore ./src/main.py --seed $seed --trust $trust --wandb_project "$wandb_project" --wandb_group "$trust" --trust_freq 25 --pretraining_rounds 100 --iterations 500 --num_clients $num_clients --eval_freq 25 --dataset $dataset_name --wandb --config_format lora --use_pretrained gpt2 --lora_mlp --lora_causal_self_attention --lora_freeze_all_non_lora
+    python -W ignore ./src/main.py --seed $seed --trust $trust --wandb_project "$wandb_project" --wandb_group "$trust" --trust_freq 25 --pretraining_rounds 100 --iterations 500 --num_clients $num_clients --eval_freq 25 --dataset $dataset_name --wandb --config_format lora --use_pretrained gpt2 --lora_mlp --lora_causal_self_attention --lora_freeze_all_non_lora --lora_rank $lora_rank
 done
 
 
