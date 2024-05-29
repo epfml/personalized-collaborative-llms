b03cf61553c1c4e0d4b361dba6eecf6eaacb2a89
Nicolas Wagner <nicolas.wagner@epfl.ch>
2024-05-29 15:33:39 +0200

    change script

--- scripts/script.sh
+++ scripts/script.sh
@@ -10,10 +10,10 @@ dataset_name=$2
 trust=$3
 num_clients=$4
 
-for seed in {1..10}; do
+for seed in {1..5}; do
     echo "---------------- New sample with seed: $seed ----------------"
 
-    python -W ignore ./src/main.py --seed $seed --trust $trust --wandb_project "$wandb_project" --wandb_group "$trust" --trust_freq 25 --pretraining_rounds 100 --iterations 500 --num_clients $num_clients --eval_freq 25 --dataset $dataset_name --wandb --config_format lora --use_pretrained gpt2 --lora_mlp --lora_causal_self_attention --lora_freeze_all_non_lora
+    python -W ignore ./src/main.py --seed $seed --trust $trust --wandb_project "$wandb_project" --wandb_group "$trust" --trust_freq 25 --pretraining_rounds 100 --iterations 500 --num_clients $num_clients --eval_freq 25 --dataset $dataset_name --wandb --config_format lora --use_pretrained gpt2 --lora_mlp --lora_causal_self_attention --lora_freeze_all_non_lora --train_size 84000000
 done
 
 
